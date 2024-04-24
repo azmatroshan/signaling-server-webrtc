136758ecec210486f0d783e0bb184a92698c2c10
Azmat Alam <azmat5star@gmail.com>
2024-04-24 14:54:05 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+
+const IO = socketIo(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+IO.use((socket, next) => {
+  if (socket.handshake.query) {
+    const callerId = socket.handshake.query.callerId;
+    socket.user = callerId;
+    next();
+  }
+});
+
+IO.on("connection", (socket) => {
+  socket.join(socket.user);
+
+  socket.on("makeCall", (data) => {
+    const calleeId = data.calleeId;
+    const sdpOffer = data.sdpOffer;
+
+    socket.to(calleeId).emit("newCall", {
+      callerId: socket.user,
+      sdpOffer: sdpOffer,
+    });
+  });
+
+  socket.on("answerCall", (data) => {
+    const callerId = data.callerId;
+    const sdpAnswer = data.sdpAnswer;
+
+    socket.to(callerId).emit("callAnswered", {
+      callee: socket.user,
+      sdpAnswer: sdpAnswer,
+    });
+  });
+
+  socket.on("IceCandidate", (data) => {
+    const calleeId = data.calleeId;
+    const iceCandidate = data.iceCandidate;
+
+    socket.to(calleeId).emit("IceCandidate", {
+      sender: socket.user,
+      iceCandidate: iceCandidate,
+    });
+  });
+
+  socket.on("disconnect", () => {
+    console.log(socket.user, "Disconnected");
+  });
+
+});
+
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
